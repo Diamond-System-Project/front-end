015d712dd079b518bfdb3a579e466368ea2c0c19
tientq-work <tientq.work@gmail.com>
2024-07-10 10:21:54 +0700

    update

--- src/api/OrderDetailAPI.jsx
+++
@@ -1,9 +0,0 @@
-import axiosClient from "./axiosClient";
-
-const OrderDetailAPI = {
-  getOrderDetailsByOrderId: (orderId) => {
-    return axiosClient.get(`/order_detail/order/${orderId}`);
-  },
-};
-
-export default OrderDetailAPI;
