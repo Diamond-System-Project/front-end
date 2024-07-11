cede2321f383a95812763ee3fa3abd90f3c6d855
tientq-work <tientq.work@gmail.com>
2024-07-11 17:47:28 +0700

    up fe

---
+++ src/api/UserAPI.jsx
@@ -0,0 +1,26 @@
+import axiosClient from "./axiosClient";
+const UserAPI = {
+  users: () => {
+    const url = `user/all`;
+    return axiosClient.get(url);
+  },
+  getUserById: (userId) => {
+    const url = `/user/${userId}`;
+    return axiosClient.get(url);
+  },
+  getUserListByRoleId: (id) => {
+    const url = `/user/role/${id}`;
+    return axiosClient.get(url);
+  },
+  updateUser: async (id, values) => {
+    const url = `/user/update/${id}`;
+    try {
+      const response = await axiosClient.put(url, values);
+      return response.data;
+    } catch (error) {
+      throw new Error(error.response.data.message || "Failed to update user");
+    }
+  },
+};
+
+export default UserAPI;
