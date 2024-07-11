7383f938e9ff5a1253549b51283d5d5e9c530106
tientq-work <tientq.work@gmail.com>
2024-07-11 21:28:36 +0700

    up

---
+++ src/App.jsx
@@ -0,0 +1,548 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./userpages/Header";
+import Footer from "./userpages/Footer";
+import Home from "./userpages/Home";
+import Register from "./userpages/Register";
+import Login from "./userpages/Login";
+import Checkout from "./userpages/Checkout";
+import ProductDetail from "./userpages/ProductDetail";
+import Cart from "./userpages/Cart";
+import PaymentMethod from "./userpages/PaymentMethod";
+import PaymentSuccess from "./userpages/PaymentSuccess";
+import SideBar from "./managerpages/SideBar";
+import Headerv2 from "./managerpages/Headerv2";
+import DashBoard from "./managerpages/DashBoard";
+import ListProduct from "./managerpages/ListProduct";
+import AddProduct from "./managerpages/AddProduct";
+import UpdateProduct from "./managerpages/UpdateProduct";
+import ManagementUser from "./managerpages/ManagementUser";
+import UserDetail from "./managerpages/UserDetail";
+import UserOrders from "./managerpages/UserOrders";
+import OrderList from "./managerpages/OrderList";
+import OrderDetail from "./managerpages/OrderDetail";
+import ManagementVoucher from "./managerpages/ManagementVoucher";
+import SideBarv2 from "./adminpages/SideBarv2";
+import ManagementEmployees from "./adminpages/ManagementEmployees";
+import StaffDetail from "./adminpages/StaffDetail";
+import SideBarv3 from "./salestaffpages/SideBarv3";
+import ListDiamond from "./salestaffpages/ListDiamond";
+import OrderListForDelivery from "./deliverystaffpages/OrderListForDelivery";
+import OrderListForSaleStaff from "./salestaffpages/OrderListForSaleStaff";
+import Carousel from "./userpages/Carousel";
+import ListProductForUser from "./userpages/ListProductForUser";
+import UserInfo from "./userpages/UserInfo";
+import OrderDetailForUser from "./userpages/OrderDetailForUser";
+import Knowledge from "./userpages/Knowledge";
+import Promotion from "./userpages/Promotion";
+import OrderHistory from "./userpages/OrderHistory";
+import ManagementStaff from "./managerpages/ManagementStaff";
+import DiamondMount from "./managerpages/DiamondMount";
+import Collection from "./userpages/Collection";
+import Quotation from "./userpages/Quotation";
+import ManagementCollection from "./managerpages/ManagementCollection";
+import ListDiamondMount from "./salestaffpages/ListDiamondMount";
+import StockList from "./salestaffpages/StockList";
+import Warranty from "./salestaffpages/Warranty";
+import ForgotPassword from "./userpages/ForgotPassword";
+import Certificate from "./managerpages/Certificate";
+import ManagementDiamond from "./managerpages/ManagementDiamond";
+import ProductPrice from "./managerpages/ProductPrice";
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Public routes */}
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <Carousel />
+              <Home />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <>
+              <Register />
+            </>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <>
+              <Login />
+            </>
+          }
+        />
+        <Route
+          path="/forgot-password"
+          element={
+            <>
+              <ForgotPassword />
+            </>
+          }
+        />
+        <Route
+          path="/reset-password"
+          element={
+            <>
+              <ForgotPassword />
+            </>
+          }
+        />
+        <Route
+          path="/order-history"
+          element={
+            <>
+              <Header />
+              <OrderHistory />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/order-detail/:id"
+          element={
+            <>
+              <Header />
+              <OrderDetailForUser />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/checkout"
+          element={
+            <>
+              <Header />
+              <Checkout />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/collection"
+          element={
+            <>
+              <Header />
+              <Collection />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/quotation"
+          element={
+            <>
+              <Header />
+              <Quotation />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/list-product"
+          element={
+            <>
+              <Header />
+              <ListProductForUser />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/product-detail/:id"
+          element={
+            <>
+              <Header />
+              <ProductDetail />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/cart"
+          element={
+            <>
+              <Header />
+              <Cart />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/payment-method"
+          element={
+            <>
+              <Header />
+              <PaymentMethod />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/payment-success"
+          element={
+            <>
+              <Header />
+              <PaymentSuccess />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <>
+              <Header />
+              <UserInfo />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/knowledge"
+          element={
+            <>
+              <Header />
+              <Knowledge />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/promotions"
+          element={
+            <>
+              <Header />
+              <Promotion />
+              <Footer />
+            </>
+          }
+        />
+
+        {/* Manager routes */}
+        <Route
+          path="/manager/dashboard"
+          element={
+            <div className="flex">
+              <SideBar />
+              <div className="flex-1">
+                <Headerv2 />
+                <DashBoard />
+              </div>
+            </div>
+          }
+        />
+        <Route
+          path="/manager/list-products"
+          element={
+            <div className="flex">
+              <SideBar />
+              <div className="flex-1">
+                <Headerv2 />
+                <ListProduct />
+              </div>
+            </div>
+          }
+        />
+        <Route
+          path="/manager/management-product-prices"
+          element={
+            <div className="flex">
+              <SideBar />
+              <div className="flex-1">
+                <Headerv2 />
+                <ProductPrice />
+              </div>
+            </div>
+          }
+        />
+        <Route
+          path="/manager/add-product"
+          element={
+            <div className="flex">
+              <SideBar />
+              <div className="flex-1">
+                <Headerv2 />
+                <AddProduct />
+              </div>
+            </div>
+          }
+        />
+        <Route
+          path="/manager/update-product/:id"
+          element={
+            <div className="flex">
+              <SideBar />
+              <div className="flex-1">
+                <Headerv2 />
+                <UpdateProduct />
+              </div>
+            </div>
+          }
+        />
+        <Route
+          path="/manager/management-user"
+          element={
+            <div className="flex">
+              <SideBar />
+              <div className="flex-1">
+                <Headerv2 />
+                <ManagementUser />
+              </div>
+            </div>
+          }
+        />
+        <Route
+          path="/manager/management-user/user-detail/:id"
+          element={
+            <div className="flex">
+              <SideBar />
+              <div className="flex-1">
+                <Headerv2 />
+                <UserDetail />
+              </div>
+            </div>
+          }
+        />
+        <Route
+          path="/manager/management-user/user-orders/:id"
+          element={
+            <div className="flex">
+              <SideBar />
+              <div className="flex-1">
+                <Headerv2 />
+                <UserOrders />
+              </div>
+            </div>
+          }
+        />
+        <Route
+          path="/manager/order-list"
+          element={
+            <div className="flex">
+              <SideBar />
+              <div className="flex-1">
+                <Headerv2 />
+                <OrderList />
+              </div>
+            </div>
+          }
+        />
+        <Route
+          path="/manager/order-list/order-detail/:id"
+          element={
+            <div className="flex">
+              <SideBar />
+              <div className="flex-1">
+                <Headerv2 />
+                <OrderDetail />
+              </div>
+            </div>
+          }
+        />
+        <Route
+          path="/manager/management-voucher"
+          element={
+            <div className="flex">
+              <SideBar />
+              <div className="flex-1">
+                <Headerv2 />
+                <ManagementVoucher />
+              </div>
+            </div>
+          }
+        />
+        <Route
+          path="/manager/management-staff"
+          element={
+            <div className="flex">
+              <SideBar />
+              <div className="flex-1">
+                <Headerv2 />
+                <ManagementStaff />
+              </div>
+            </div>
+          }
+        />
+        <Route
+          path="/manager/diamond-mount"
+          element={
+            <div className="flex">
+              <SideBar />
+              <div className="flex-1">
+                <Headerv2 />
+                <DiamondMount />
+              </div>
+            </div>
+          }
+        />
+        <Route
+          path="/manager/collection"
+          element={
+            <div className="flex">
+              <SideBar />
+              <div className="flex-1">
+                <Headerv2 />
+                <ManagementCollection />
+              </div>
+            </div>
+          }
+        />
+        <Route
+          path="/manager/certificate"
+          element={
+            <div className="flex">
+              <SideBar />
+              <div className="flex-1">
+                <Headerv2 />
+                <Certificate />
+              </div>
+            </div>
+          }
+        />
+        <Route
+          path="/manager/management-diamond"
+          element={
+            <div className="flex">
+              <SideBar />
+              <div className="flex-1">
+                <Headerv2 />
+                <ManagementDiamond />
+              </div>
+            </div>
+          }
+        />
+        {/* Sale Staff routes */}
+        <Route
+          path="/staff/list-diamond"
+          element={
+            <div className="flex">
+              <SideBarv3 />
+              <div className="flex-1">
+                <Headerv2 />
+                <ListDiamond />
+              </div>
+            </div>
+          }
+        />
+        <Route
+          path="/staff/list-diamond-mount"
+          element={
+            <div className="flex">
+              <SideBarv3 />
+              <div className="flex-1">
+                <Headerv2 />
+                <ListDiamondMount />
+              </div>
+            </div>
+          }
+        />
+        <Route
+          path="/staff/stock-list"
+          element={
+            <div className="flex">
+              <SideBarv3 />
+              <div className="flex-1">
+                <Headerv2 />
+                <StockList />
+              </div>
+            </div>
+          }
+        />
+        <Route
+          path="/staff/order-list"
+          element={
+            <div className="flex">
+              <SideBarv3 />
+              <div className="flex-1">
+                <Headerv2 />
+                <OrderListForSaleStaff />
+              </div>
+            </div>
+          }
+        />
+        <Route
+          path="/staff/order-list/order-detail/:id"
+          element={
+            <div className="flex">
+              <SideBarv3 />
+              <div className="flex-1">
+                <Headerv2 />
+                <OrderDetail />
+              </div>
+            </div>
+          }
+        />
+        <Route
+          path="/staff/warranty"
+          element={
+            <div className="flex">
+              <SideBarv3 />
+              <div className="flex-1">
+                <Headerv2 />
+                <Warranty />
+              </div>
+            </div>
+          }
+        />
+        {/* Delivery Staff routes */}
+        <Route
+          path="/delivery"
+          element={
+            <div className="flex flex-col">
+              <Headerv2 />
+              <OrderListForDelivery />
+            </div>
+          }
+        />
+        {/* Admin routes */}
+        <Route
+          path="/admin/dashboard"
+          element={
+            <div className="flex">
+              <SideBarv2 />
+              <div className="flex-1">
+                <Headerv2 />
+                <DashBoard />
+              </div>
+            </div>
+          }
+        />
+        <Route
+          path="/admin/management-employees"
+          element={
+            <div className="flex">
+              <SideBarv2 />
+              <div className="flex-1">
+                <Headerv2 />
+                <ManagementEmployees />
+              </div>
+            </div>
+          }
+        />
+        <Route
+          path="/admin/employees-detail/:id"
+          element={
+            <div className="flex">
+              <SideBarv2 />
+              <div className="flex-1">
+                <Headerv2 />
+                <StaffDetail />
+              </div>
+            </div>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/managerpages/OrderDetail.jsx
@@ -0,0 +1,132 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Card, Col, Row, Table, Typography, Spin, message } from "antd";
+import OrderDetailAPI from "../api/OrderDetailAPI";
+
+const { Title, Text } = Typography;
+
+const OrderDetail = () => {
+  const { id } = useParams(); // Get order ID from URL params
+  const [order, setOrder] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (!id) {
+      message.error("Order ID is not provided.");
+      return;
+    }
+
+    const fetchOrderDetails = async () => {
+      try {
+        const response = await OrderDetailAPI.getOrderDetailsByOrderId(id);
+        console.log("API response:", response); // Log the entire response
+
+        // Extract the nested data
+        if (
+          response.data &&
+          response.data.data &&
+          response.data.data.length > 0
+        ) {
+          setOrder(response.data.data[0]); // Assuming response.data.data is an array
+        } else {
+          console.error("Unexpected data format:", response.data); // Log the unexpected format
+          message.error("Failed to fetch order details.");
+        }
+      } catch (error) {
+        console.error("Error fetching order details:", error);
+        message.error("Failed to fetch order details.");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchOrderDetails();
+  }, [id]);
+
+  if (loading) {
+    return <Spin tip="Loading..." />;
+  }
+
+  if (!order) {
+    return <p>No order details found.</p>;
+  }
+
+  const columns = [
+    {
+      title: "Product Name",
+      dataIndex: "productName",
+      key: "productName",
+    },
+    {
+      title: "Quantity",
+      dataIndex: "quantity",
+      key: "quantity",
+    },
+    {
+      title: "Price",
+      dataIndex: "price",
+      key: "price",
+    },
+  ];
+
+  const orderInfo = order.orderId;
+  const products = [
+    {
+      key: 1,
+      productName: order.productId.productName,
+      quantity: order.quantity,
+      price: order.productId.price * order.quantity,
+    },
+  ];
+
+  return (
+    <div className="p-6 bg-gray-100">
+      <Card className="mb-6 shadow">
+        <Row gutter={16} justify="space-between" align="middle">
+          <Col>
+            <Title level={4}>Order ID: {orderInfo.orderId}</Title>
+            <Text>Date: {orderInfo.order_date}</Text>
+          </Col>
+          <Col>
+            <Text>Status: {orderInfo.status}</Text>
+          </Col>
+        </Row>
+      </Card>
+
+      <Row gutter={16} className="mb-6 h-fit">
+        <Col span={8}>
+          <Card title="Customer" className="shadow">
+            <p>Full Name: {orderInfo.cname}</p>
+            <p>Email: {orderInfo.email}</p>
+            <p>Phone: {orderInfo.phone}</p>
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card title="Order Info" className="shadow">
+            <p>Payment Method: {orderInfo.payment_method}</p>
+            <p>Status: {orderInfo.status}</p>
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card title="Deliver to" className="shadow h-fit">
+            <p>Address: {orderInfo.address}</p>
+          </Card>
+        </Col>
+      </Row>
+
+      <Card title="Products" className="shadow">
+        <Table columns={columns} dataSource={products} pagination={false} />
+        <div className="flex justify-end mt-4">
+          <div className="w-full md:w-1/4">
+            <div className="flex justify-between font-semibold text-lg py-1">
+              <span>Total:</span>
+              <span>{orderInfo.payment}</span>
+            </div>
+          </div>
+        </div>
+      </Card>
+    </div>
+  );
+};
+
+export default OrderDetail;
